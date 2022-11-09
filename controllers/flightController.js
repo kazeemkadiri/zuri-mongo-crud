2a4be0060c371db095fc9bd6d75b800a174917d4
Kazeem Abolaji Kadiri <kazeem.kadiri@gmail.com>
2022-11-09 15:18:14 +0100

    remove files that are no longer needed

--- controllers/flightController.js
+++
@@ -1,6 +0,0 @@
-exports.example = (req, res) => {
-    console.log("example")
-    res.send("Flight example")
-}
-
-
--- routes/flightRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-const controller = require('../controllers/flightController');
-
-router.get('/', controller.example)
-
-module.exports = router;
-
